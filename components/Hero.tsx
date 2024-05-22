a3fca9009e4ebdbc138e60febaee4675f4fcd8b6
YanBerdin <yandevformation@gmail.com>
2024-05-23 00:36:56 +0200

    Feat MagicButton

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,8 @@
-// import React from "react";
+import React from "react";
+import { FaLocationArrow } from "react-icons/fa6";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
-
+import { MagicButton } from "./ui/MagicButton";
 
 export const Hero = () => {
     return (
@@ -11,20 +12,49 @@ export const Hero = () => {
                 <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
                 <Spotlight className="-top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
             </div>
+
+            {/*
+            *  UI: grid
+            *  change bg color to bg-black-100 and reduce grid color from
+            *  0.2 to 0.03
+            */}
+
             <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+
                 {/* Radial gradient for the container to give a faded look */}
-                <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100  bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
+
+                <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100  bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+                // change the bg to bg-black-100, so it matches the bg color and will blend in
+                />
+
             </div>
             <div className="flex justify-center relative my-20 z-10">
                 <div className="max-w-[89vh] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-                    <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+                    <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
                         Dynamic Web Magic With Next.js
-                    </h2>
+                    </p>
+                    {/**
+                     *  Link: https://ui.aceternity.com/components/text-generate-effect
+                     *
+                     *  change md:text-6xl, add more responsive code
+                     */}
                     <TextGenerateEffect
-                    className="text-center text-[40px] md:text-5xl lg:text-6xl"
+                        className="text-center text-[40px] md:text-5xl lg:text-6xl"
                         words="Transforming Concepts into Seamless Experiences"
                     />
-                    <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi, I&apos;m Yan, a React.js Developer based in France.</p>
+                    <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+                        Hi, I&apos;m Yan, a React.js Developer based in France.
+                    </p>
+
+
+                        <MagicButton
+                            title="Show My Work"
+                            icon={<FaLocationArrow />}
+                            position="right"
+                            // handleClick={() => console.log('Button clicked!')}
+                            otherClasses="custom-class"
+                            href="#projects"
+                        />
                 </div>
             </div>
         </div>
