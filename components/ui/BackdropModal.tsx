d4db55ababbc0c4cf7a97db80f3cb41fbb47c0a7
YanBerdin <yandevformation@gmail.com>
2024-06-06 02:59:54 +0200

    Receive id as props in BackdropModal from BentoGrid For dynamic display of the modal

--- components/ui/BackdropModal.tsx
+++ components/ui/BackdropModal.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
 
-export default function BackdropModal() {
+import { gridItems } from "../../data/index";
+// afficher le contenu de gridItems dans la modal 
+
+
+export default function BackdropModal({ id }: { id: number }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [backdrop, setBackdrop] = React.useState<"blur" | "transparent" | "opaque" | "backdrop-blur-md" | undefined>('transparent') as any[];
+  const [title, settitle] = React.useState("");
+  const [description, setDescription] = React.useState("");
 
   const backdrops = ["blur"];
 
-  const handleOpen = (backdrop: string) => { // Explicitly type the 'backdrop' parameter as a string
-    setBackdrop(backdrop)
+  const handleOpen = (backdrop: string, id: number) => { // Explicitly type the 'backdrop' parameter as a string
+    setBackdrop(backdrop);
+    const item = gridItems.find(item => item.id === id);
+    if (item) {
+      settitle(item.title);
+      setDescription(item.description);
+    }
     onOpen();
   }
 
@@ -20,7 +31,7 @@ export default function BackdropModal() {
             key={b}
             variant="flat"
             color="warning"
-            onPress={() => handleOpen(b)}
+            onPress={() => handleOpen(b, id)} // Pass both 'b' and 'id' as parameters
             className="capitalize text-sm"
           >
             {/*{b}*/}
@@ -40,11 +51,13 @@ export default function BackdropModal() {
             borderRadius: `calc(1.75rem* 0.96)`,
           }}
           // remove bg-white dark:bg-slate-900
-          className=" border-neutral-200 dark:border-slate-800 py-4"
+          className=" border-neutral-200 dark:border-slate-800"
         >
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
+              <ModalHeader className="flex flex-col gap-1">
+                <h2 className="mt-2">{title}</h2>
+              </ModalHeader>
               <ModalBody>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -55,13 +68,6 @@ export default function BackdropModal() {
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Nullam pulvinar risus non risus hendrerit venenatis.
                   Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit
-                  dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis.
-                  Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod.
-                  Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur
-                  proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
                 </p>
               </ModalBody>
               <ModalFooter>
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -2,16 +2,12 @@
 
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
-
 // Also install this npm i --save-dev @types/react-lottie
-//import Lottie from "react-lottie";
-
+// import Lottie from "react-lottie";
 import { cn } from "@/lib/utils/cn";
-
-
 import { BackgroundGradientAnimation } from "./GradientBg";
 import { GridGlobe } from "./GridGlobe";
-//import animationData from "@/data/confetti.json";
+// import animationData from "@/data/confetti.json";
 import { MagicButton } from "../MagicButton";
 
 import Image from "next/image";
@@ -202,8 +198,8 @@ export const BentoGridItem = ({
                     )}
 
                     {(id === 1 || id === 4 || id === 5) && (
-                        <div>
-                            <BackdropModal />
+                        <div className="mt-4">
+                            <BackdropModal id={id} />
                         </div>
                     )}
 
