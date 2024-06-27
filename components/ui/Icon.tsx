85a3925a089dd77bec055b8caab22a2562f636c0
YanBerdin <yandevformation@gmail.com>
2024-06-27 22:44:33 +0200

    fix property  error stroke-width

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -57,11 +57,11 @@ const DemoIcon = (props: SVGProps<SVGSVGElement>) => (
     width='24'
     height='24'
     viewBox='0 0 24 24'
-    stroke-width='2'
+    strokeWidth='2'
     stroke='currentColor'
     fill='none'
-    stroke-linecap='round'
-    stroke-linejoin='round'
+    strokeLinecap='round'
+    strokeLinejoin='round'
   >
     <path stroke='none' d='M0 0h24v24H0z' fill='none' />
     <path d='M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5' />
@@ -77,17 +77,17 @@ const ResumeIcon = (props: SVGProps<SVGSVGElement>) => (
     width='24'
     height='24'
     viewBox='0 0 24 24'
-    stroke-width='2'
+    strokeWidth='2'
     stroke='currentColor'
     fill='none'
-    stroke-linecap='round'
-    stroke-linejoin='round'
+    strokeLinecap='round'
+    strokeLinejoin='round'
   >
-    <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
-    <path d='M14 3v4a1 1 0 0 0 1 1h4'></path>
-    <path d='M5 8v-3a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2h-5'></path>
+    <path stroke='none' d='M0 0h24v24H0z' fill='none'/>
+    <path d='M14 3v4a1 1 0 0 0 1 1h4'/>
+    <path d='M5 8v-3a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2h-5'/>
     <circle cx='6' cy='14' r='3'></circle>
-    <path d='M4.5 17l-1.5 5l3 -1.5l3 1.5l-1.5 -5'></path>
+    <path d='M4.5 17l-1.5 5l3 -1.5l3 1.5l-1.5 -5'/>
   </svg>
 );
 
