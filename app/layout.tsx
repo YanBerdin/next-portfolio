bb5ab3e9fb89a41e1c1b28f494572ffe640fea54
YanBerdin <yandevformation@gmail.com>
2024-07-02 21:52:56 +0200

    update  metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,16 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Yan's Portfolio",
-  description: "Web Developper Portfolio",
+  description: "Web Developper",
+  metadataBase: new URL('https://yanberdin.com'),
+  alternates: {
+    canonical: '/',
+  },
+  referrer: 'origin-when-cross-origin',
+  robots: 'index, follow',
+  keywords: ['Next.js', 'React', 'JavaScript', 'TypeScript', 'Tailwind CSS', 'Yan Berdin', 'Portfolio'],
+  authors: [{ name: 'Yan Berdin', url: 'https://yanberdin.com' }],
+  creator: 'Yan Berdin',
 };
 
 export default function RootLayout({
@@ -18,15 +27,15 @@ export default function RootLayout({
   return (
     <html lang="fr" suppressHydrationWarning>
       <body className={inter.className}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        // themes={['dark, light, system']}
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          // themes={['dark, light, system']}
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
