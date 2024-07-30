3b1985a209b44de6b89c3326c857eddd9ec9df0f
YanBerdin <yandevformation@gmail.com>
2024-07-30 20:22:48 +0200

    update About section

--- components/About.tsx
+++ components/About.tsx
@@ -28,17 +28,26 @@ export const About = () => {
               </p>
 
               <p className="text-sm sm:text-base xl:text-lg text-muted-foreground mt-4 ">
-                Mon profil Fullstack s&apos;est consolidé par la pratique, grâce à l&apos;implémentation de fonctionnalités utilisateur avancées côté client et à la sécurisation contre diverses attaques en ligne côté serveur, témoignant de ma capacité à créer des solutions digitales robustes et fonctionnelles.
+                Fort d&apos;une formation Fullstack de niveau 5 (Bac+2) et d&apos;une expérience pratique acquise à travers plusieurs projets, j&apos;ai réorienté ma carrière du e-commerce vers le développement web.
+              </p>
+
+              <p className="text-sm sm:text-base xl:text-lg text-muted-foreground mt-4 ">
+                Déterminé à poursuivre cette montée en compétence. Je recherche une entreprise où m&apos;investir et dont l&apos;encadrement me permettra d&apos;accroitre mon expertise pour contribuer efficacement à ses projets. Focalisé sur cet objectif, j&apos;accorde une attention particulière à la sécurité des données, la maintenabilité et l&apos;accessibilité des applications.
+              </p>
+
+              <p className="text-sm sm:text-base xl:text-lg text-muted-foreground mt-4 ">Parallèlement, mon expérience précédente dans la vente et le e-commerce a façonné mon approche centrée sur les utilisateurs. </p>
+
+              <p className="text-sm sm:text-base xl:text-lg text-muted-foreground mt-4">
+                <strong>Gestion de projets, analyse de données et relation client</strong> : En tant que gérant e-commerce, j&apos;ai piloté avec succès la croissance d&apos;une activité en ligne générant 15 000 commandes annuelles, optimisé les processus, suivi les indicateurs de performance et assuré une relation client de qualité.
               </p>
 
               <p className="text-sm sm:text-base xl:text-lg text-muted-foreground mt-4">
-                Parallèlement, mon expérience précédente dans la vente et le e-commerce a façonné mon approche centrée sur les utilisateurs, me permettant de comprendre et de répondre efficacement aux besoins des clients dans des environnements variés.
+                <strong>Agilité, collaboration et optimisation de processus</strong> : En tant que vendeur puis manager chez Levi&apos;s, j&apos;ai animé une équipe de 8 collaborateurs et amélioré le chiffre d&apos;affaires de 15% par an avec des stratégies pour optimiser les processus et améliorer la satisfaction client.
               </p>
 
-              <p className="text-sm sm:text-base xl:text-lg text-muted-foreground mt-4">
-                Ma transition vers le développement web est guidée par une passion pour la programmation, inspirée par mon frère architecte, ainsi que par un engagement à contribuer positivement aux projets d&apos;équipe et aux objectifs de l&apos;entreprise.
-              </p>
+              <p className="text-sm sm:text-base xl:text-lg text-muted-foreground mt-4 "><strong>Adaptabilité et apprentissage continu</strong> : Mon parcours m&apos;a permis de développer une grande adaptabilité et une capacité à apprendre rapidement de nouvelles compétences. De la vente en magasin à la gestion d&apos;une activité en ligne, j&apos;ai toujours su m&apos;adapter à de nouveaux environnements et relever de nouveaux défis. Cette polyvalence est un atout précieux pour un poste de développeur en constante évolution.</p>
 
+              <p className="text-sm sm:text-base xl:text-lg text-muted-foreground mt-4 ">Ces compétences combinées à ma passion pour la programmation inspirée par mon frère architecte de solutions, me permettent de contribuer positivement aux projets d&apos;équipe et aux objectifs métiers.</p>
             </div>
             <IconCloudComp />
             {/*<Statistics />*/}
